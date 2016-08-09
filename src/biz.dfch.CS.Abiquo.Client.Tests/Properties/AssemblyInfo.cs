50f9ba2a54499e42587a3ee6445b86e83c4b7d6a
rufer7 <marc.rufer@d-fens.net>
2016-08-09 08:13:08 +0200

    Create common AssemblyVersion file to set Assembly Version in one place
    resolves #8

--- src/biz.dfch.CS.Abiquo.Client.Tests/Properties/AssemblyInfo.cs
+++ src/biz.dfch.CS.Abiquo.Client.Tests/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8273a929-b3b8-4f09-8c29-98da126221ed")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
