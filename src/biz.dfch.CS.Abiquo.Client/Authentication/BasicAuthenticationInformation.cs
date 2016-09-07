7b0765bcc1cb9f05a626af805d16f29da19580af
rufer7 <marc.rufer@d-fens.net>
2016-09-07 09:51:38 +0200

    minor

--- src/biz.dfch.CS.Abiquo.Client/Authentication/BasicAuthenticationInformation.cs
+++ src/biz.dfch.CS.Abiquo.Client/Authentication/BasicAuthenticationInformation.cs
@@ -18,8 +18,6 @@
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Linq;
-﻿using System.Net.Http;
-﻿using System.Net.Http.Headers;
 ﻿using System.Text;
 using System.Threading.Tasks;
 
