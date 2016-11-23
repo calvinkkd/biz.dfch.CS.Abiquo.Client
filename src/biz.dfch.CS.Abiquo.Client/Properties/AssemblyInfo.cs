c17c13a2d685d0c13fb9079021548b271fc6ca06
Ronald Rink <ronald.rink@d-fens.net>
2016-11-23 10:24:47 +0100

    misc
    * upgraded biz.dfch.CS.Web.Utilities to 3.0.0
    * added strong name to assemblies
    * linked AssemblyVersion to projects

--- src/biz.dfch.CS.Abiquo.Client/Properties/AssemblyInfo.cs
+++ src/biz.dfch.CS.Abiquo.Client/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("d-fens GmbH")]
 [assembly: AssemblyProduct("biz.dfch.CS.Abiquo.Client")]
 [assembly: AssemblyCopyright("Copyright © 2016 d-fens GmbH")]
-[assembly: AssemblyTrademark("The d-fens logo is a registered trademark in the European Union and/or other countires.")]
+[assembly: AssemblyTrademark("The d-fens logo is a registered trademark in the European Union and/or other countries.")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -22,5 +22,22 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ef1c86d1-2bc5-4742-b963-cfbad1f7ea02")]
 
-[assembly: InternalsVisibleTo("biz.dfch.CS.Abiquo.Client.Tests")]
-[assembly: InternalsVisibleTo("biz.dfch.PS.Abiquo.Client.Tests")]
+[assembly:InternalsVisibleTo("biz.dfch.CS.Abiquo.Client.Tests" +
+",PublicKey=" +
+"0024000004800000940000000602000000240000525341310004000001000100b3ff1f5b1cd339" +
+"a46db108a12f4a03c7cef0c469a649a1af9c3853730c054e65e5f95a697f46998c3cd3ba7fae75" +
+"5cb5556b570ab64daa3e7f720a27331a3334c39510ed49c4b253dcb3f7716000fd836c8c2dbdb5" +
+"153efeff6e70570bf6d814ff18272afac8750fb7f1ccd13616597b2828e5ef297043c5e0bf4bbc" +
+"101bdcae" +
+""
+    )]
+
+[assembly:InternalsVisibleTo("biz.dfch.PS.Abiquo.Client.Tests" +
+",PublicKey=" +
+"0024000004800000940000000602000000240000525341310004000001000100b3ff1f5b1cd339" +
+"a46db108a12f4a03c7cef0c469a649a1af9c3853730c054e65e5f95a697f46998c3cd3ba7fae75" +
+"5cb5556b570ab64daa3e7f720a27331a3334c39510ed49c4b253dcb3f7716000fd836c8c2dbdb5" +
+"153efeff6e70570bf6d814ff18272afac8750fb7f1ccd13616597b2828e5ef297043c5e0bf4bbc" +
+"101bdcae" +
+""
+    )]
