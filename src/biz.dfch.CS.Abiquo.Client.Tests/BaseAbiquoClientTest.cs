a41c6bebb9f922ae79ec64decfb44660c0026724
rufer7 <marc.rufer@d-fens.net>
2016-12-23 14:31:11 +0100

    minor

--- src/biz.dfch.CS.Abiquo.Client.Tests/BaseAbiquoClientTest.cs
+++ src/biz.dfch.CS.Abiquo.Client.Tests/BaseAbiquoClientTest.cs
@@ -356,7 +356,7 @@ namespace biz.dfch.CS.Abiquo.Client.Tests
 
             // Assert
             Assert.AreEqual("Arbitrary-Result", result);
-            Assert.IsNull(sut.SessionToken);
+            Assert.AreEqual(SESSION_TOKEN, sut.SessionToken);
 
             Mock.Assert(responseHeaders);
             Mock.Assert(restCallExecutor);
